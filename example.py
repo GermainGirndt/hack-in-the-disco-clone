5190dcc79f2063d2059b4c33145bea738eaad49d
aritrey <51917383+aritrey@users.noreply.github.com>
2023-12-29 15:08:19 +0100

    add heart code

--- example.py
+++ example.py
@@ -18,6 +18,64 @@ values = {
 }
 
 
+
+def sendHeart():
+
+    heartPositions = [
+            (0.0, 5.0),
+            (0.55, 7.01),
+            (3.71, 10.71),
+            (9.39, 11.77),
+            (14.58, 8.4),
+            (15.84, 2.42),
+            (12.29, -3.64),
+            (6.37, -8.98),
+            (1.73, -13.6),
+            (0.07, -16.58),
+            (-0.07, -16.58),
+            (-1.73, -13.6),
+            (-6.37, -8.98),
+            (-12.29, -3.64),
+            (-15.84, 2.42),
+            (-14.58, 8.4),
+            (-9.39, 11.77),
+            (-3.71, 10.71),
+            (-0.55, 7.01),
+            (-0.0, 5.0),
+    ]
+
+    MAX_RANGE = 21
+    for i in range(0, MAX_RANGE):
+
+        DELTA = 1
+        variation = random.randint(-DELTA, DELTA)/100
+
+        new_intensity = light_parameters["intensity"] + \
+            (light_parameters["intensity"] * variation)
+        new_color = light_parameters["color"] + \
+            (light_parameters["color"] * variation)
+        new_frost = light_parameters["frost"] + \
+            (light_parameters["frost"] * variation)
+
+        position_x = TRACKER_X + str((heartPositions[i-1][0]) * 0.2)
+        position_y = TRACKER_Y + str((heartPositions[i-1][1]) * 0.2)
+        # print(f"Debugging X: {position_x}")
+        # print(f"Debugging Y: {position_y}")
+
+        client.send_message(f"/light{i}/xpos", position_x)
+        client.send_message(f"/light{i}/ypos", position_y)
+        client.send_message(f"/light{i}/intensity", new_intensity)
+        client.send_message(f"/light{i}/color", new_color)
+        client.send_message(f"/light{i}/frost", new_frost)
+        client.send_message(f"/light{i}/height", 0.0)
+
+        light_parameters["intensity"] = new_intensity
+        light_parameters["color"] = new_color
+        light_parameters["frost"] = new_frost
+
+
+
+
 def update_handler(address, *args):
     values[str(address)] = args[0]
 
@@ -74,7 +132,7 @@ def send():
 async def loop():
     while True:
         print(f"X: {values[TRACKER_X]}\tY: {values[TRACKER_Y]}")
-        send()
+        sendHeart()
         await asyncio.sleep(0.1)
 
 
